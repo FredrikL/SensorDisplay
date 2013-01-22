2f5c1a3b2a0bdc2ba8e9c7da2fa038da58a00af9
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-01-22 16:21:53 +0100

    added open file dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    int x = 0;
-    auto foo = 1;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::openFile() {
+    qDebug() << "openFile()";
+    auto fileName = QFileDialog::getOpenFileName(this, tr("Open log file"), "", tr("Log file (*.txt)"));
+    qDebug() << fileName;
 }
